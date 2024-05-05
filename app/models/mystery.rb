c9f8d6202956639bbb5daa04d793b5d90ad0ce4e
yamadahideto <yahideto428@gmail.com>
2024-05-05 23:32:23 +0900

    モデルの不要コードの削除

--- app/models/mystery.rb
+++ app/models/mystery.rb
@@ -1,5 +1,4 @@
 class Mystery < ApplicationRecord
-  # after_initialize :set_default_value
   has_one_attached :image
   belongs_to :user
   belongs_to :genre
@@ -9,9 +8,4 @@ class Mystery < ApplicationRecord
   validates :image, presence: true, content_type: ['image/png', 'image/jpg', 'image/jpeg', 'image/webp', 'image/HEIF']
   validates :content, presence: true
   validates :correct_answer, presence: true
-
-  # def set_default_value
-  #   self.title = 'AI生成'
-  #   self.content = 'AI生成'
-  # end
 end
