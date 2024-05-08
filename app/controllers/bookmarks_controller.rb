2006d119e7df883d22cfced92c04085375863603
yamadahideto <yahideto428@gmail.com>
2024-05-08 19:53:49 +0900

    ブックマーク機能のAjax化

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,13 +1,11 @@
 class BookmarksController < ApplicationController
   def create
-    mystery = Mystery.find(params[:mystery_id])
-    current_user.bookmark(mystery)
-    redirect_to mysteries_path
+    @mystery = Mystery.find(params[:mystery_id])
+    current_user.bookmark(@mystery)
   end
 
   def destroy
-    mystery = current_user.bookmarks.find_by(mystery_id: params[:id]).mystery
-    current_user.unbookmark(mystery)
-    redirect_to mysteries_path
+    @mystery = current_user.bookmarks.find_by(mystery_id: params[:id]).mystery
+    current_user.unbookmark(@mystery)
   end
 end
