3e3aa192ae09f8746d5f76db1f1e0029ff448386
yamadahideto <yahideto428@gmail.com>
2024-05-24 01:17:26 +0900

    AI謎生成時と自作の謎投稿時でバリデーションを切り替えられるように修正

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -3,13 +3,15 @@ import { Controller } from "@hotwired/stimulus";
 // Connects to data-controller="modal"
 export default class extends Controller {
   connect() {
-    console.log("テスト");
     document.addEventListener("turbo:submit-start", () => {
       this.showModal();
     });
 
     document.addEventListener("turbo:submit-end", () => {
       this.hideModal();
+    });
+    document.addEventListener("turbo:load", () => {
+      this.switchForm();
     });
   }
 
@@ -19,5 +21,42 @@ export default class extends Controller {
 
   hideModal() {
     document.getElementById("modal").classList.add("hidden");
+  }
+
+  switchForm() {
+    const aiGenerateButton = document.getElementById("ai_generate_button");
+    const imagePostButton = document.getElementById("image_post_button");
+    const aiGenerateForm = document.getElementById("ai_generate_form");
+    const imagePostForm = document.getElementById("image_post_form");
+
+    const toggleFormVisibility = (
+      buttonToShow,
+      buttonToHide,
+      formToShow,
+      formToHide
+    ) => {
+      buttonToShow.style.display = "block";
+      buttonToHide.style.display = "none";
+      formToShow.style.display = "block";
+      formToHide.style.display = "none";
+    };
+
+    imagePostButton.addEventListener("click", () => {
+      toggleFormVisibility(
+        aiGenerateButton,
+        imagePostButton,
+        imagePostForm,
+        aiGenerateForm
+      );
+    });
+
+    aiGenerateButton.addEventListener("click", () => {
+      toggleFormVisibility(
+        imagePostButton,
+        aiGenerateButton,
+        aiGenerateForm,
+        imagePostForm
+      );
+    });
   }
 }
