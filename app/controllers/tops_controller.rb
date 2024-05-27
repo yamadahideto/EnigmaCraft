5895c27f0d3482993f635563f3b3008618c25922
yamadahideto <yahideto428@gmail.com>
2024-05-27 18:01:43 +0900

    利用規約・プライバシーポリシーの作成

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -1,4 +1,8 @@
 class TopsController < ApplicationController
-  skip_before_action :require_login, only: %i[index]
+  skip_before_action :require_login, only: %i[index privacy terms]
   def index; end
+
+  def privacy; end
+
+  def terms; end
 end
